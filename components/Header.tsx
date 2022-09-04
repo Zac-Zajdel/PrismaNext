66a6a5fe8c76a5b6759ddd33ac3e76055bca1716
Zac-Zajdel <zaczajdel213@gmail.com>
2022-09-04 17:20:57 -0500

    feat: More setup for header and login

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,7 @@
-import { useRouter } from 'next/router'
 import { useState } from 'react'
+import Link from 'next/link'
 
 const Header = () => {
-  const router = useRouter()
   const [isToggled, setIsToggled] = useState(false)
 
   return (
@@ -17,12 +16,11 @@ const Header = () => {
           </span>
         </a>
         <div className="flex items-center lg:order-2">
-          <a
-            className="cursor-pointer text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            onClick={() => router.push('/login')}
-          >
-            Log in
-          </a>
+          <Link href="/login">
+            <span className="cursor-pointer text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+              Log in
+            </span>
+          </Link>
           <button
             type="button"
             className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -61,6 +59,13 @@ const Header = () => {
           } justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
         >
           <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <li className="cursor-pointer">
+              <Link href="/">
+                <span className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                  Dashboard
+                </span>
+              </Link>
+            </li>
             <li>
               <a
                 href="https://nextjs.org"
